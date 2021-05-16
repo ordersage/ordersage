fad89c1962b21b69ede56bcc9e47657f44e40346
CImburgia <carina.imburgia@utah.edu>
2021-05-15 20:41:34 -0600

    Started ssh connect method

--- simple_tool.py
+++ simple_tool.py
@@ -5,9 +5,9 @@
 # 2. Parse arguments
 # 3. Establish connection to pre-allocated machine
 # 4. Get remote server ready for experiments
-#       a. Install packages, clone repo,
-#          set up directory for results, reboot to clean state
-
+#       a. clone repo, set up directory for results
+#       b. run initialization script
+#       c. reboot to clean state
 # On remote machine (facilitated by controller node)
 # 5. Run loop n times:
 #   a. Run experiments in fixed order
@@ -21,17 +21,80 @@
 
 # On controller node:
 # 8. Insert results into database
+
 # *Eventually add in stats tests to tell if order matters
-# *Function for rebooting? How to check for reboot completion.
+
+# system libraries
+import sys
+import argparse
+
+# SSH library
+import paramkio
+
+# Subprocess functions
+from subprocess import Popen,PIPE,STDOUT,call
 
 def parseArgs():
-    pass
 
-# Looking at libraries like paramiko to execute a single cmd or list of commands
-# Does it make sense in parts of the script to invoke a shell to use stdin/out
-# channels multiple times in one connection?
 def openSSHConnection(hostname, username, portnum, keypath):
-    pass
+    ssh = paramiko.SSHClient()
+    sshkey = paramiko.RSAKey.from_private_key_file(keypath)
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # SSH Connect
+    nTries = 0
+    maxTries = 3
+    while True:
+        try:
+            ssh.connect(hostname = hostname, port = portnum, username = username, pkey = sshkey)
+        except Exception as e:
+            nTries += 1
+            print "In openSSHConnection: " + repr(e) + " - " + str(e)
+            print "Error #" + str(nTries) + " out of " + str(maxTries) + "."
+            if nTries >= maxTries:
+                return "Failure"
+            else:
+                sleep(10)
+                print "\tRetrying..."
+        else:
+            print "SSH connection to " + hostname + " successful."
+            try:
+                # open session and return the channel
+                transport = ssh.get_transport()
+                return transport.open_session()
+            except Exception as e:
+                print "In openSSHConnection: " + repr(e) + " - " + str(e)
+                return "Failure"
+            else:
+                break
+
+
+def checkReboot(hostname):
+    # Spin until the machine comes up and is ready for SSH
+    nTries = 0
+    maxTries = 8
+    print "Awaiting completion of reboot for " + hostname + ", sleeping for 4 minutes..."
+    sleep(240)
+    while True:
+        try:
+            out = Popen(["nc", "-z", "-v", "-w5", server, "22"],stderr=STDOUT,stdout=PIPE)
+        except:
+            print "In rebootRemoteServer: " + repr(e) + " - " + str(e)
+            return "Failure"
+        else:
+            t = out.communicate()[0],out.returncode
+            if t[1] == 0:
+                break
+            else:
+                nTries += 1
+                if nTries > maxTries:
+                    return "Failure"
+                else:
+                    print "\tConnection attempt to " + hostname + " timed out, retrying (" + str(nTries) + " out of " + str(maxTries) + ")..."
+                    sleep(60)
+
+    print "Node " + hostname + " is up at " + str(datetime.datetime.today()) + ", connecting via SSH."
+    return "Success"
 
 # Clone repo for fixed and random, set up results directory
 def initializeRemoteServer(repo, dest_dir):
@@ -52,4 +115,12 @@ def connectToDatabase(hostname, username, password, database):
     pass
 
 def main():
-    pass
+    openSSHConnection("ms0745.utah.cloudlab.us", 22, "carina", )
+
+
+# Entry point of the application
+if __name__ == "__main__":
+    main()
+else:
+    print "Error, cannot enter main, exiting."
+    sys.exit(2)
