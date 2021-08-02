cec60b6d523b836fbcadcb3f712c11eb19445475
Carina Imburgia <Carina.Imburgia@utah.edu>
2021-08-02 13:15:04 -0600

    Refactored logging slightly

--- controller.py
+++ controller.py
@@ -19,6 +19,7 @@ from scp import SCPClient
 # error handling and logging
 from requests import ConnectionError
 import logging
+from logger import configure_logging
 
 # Subprocess functions
 from subprocess import Popen, PIPE, STDOUT, run
@@ -42,38 +43,6 @@ from configparser import ConfigParser
 
 # See debug info from paramiko
 logging.getLogger("paramiko").setLevel(logging.DEBUG)
-
-########################
-### Configure Log file ###
-########################
-def configure_logging(debug=False, filename='mylog.log'):
-    """ This function configures logging facility.
-    The current setup is for printing log messages onto console AND onto the file.
-    Formatters are the same for both output destinations.
-    Handing of log levels:
-    - console output includes DEBUG messages or not depending on the `debug` argument.
-    - file ouput includes all levels including DEBUG.
-    """
-    frmt_str = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
-    frmt_out = '%(message)s'
-
-    # set up logging to file
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler(filename)
-    f_formatter = logging.Formatter(frmt_str)
-    file_handler.setFormatter(f_formatter)
-    logger.addHandler(file_handler)
-
-    # define a handler for console
-    console = logging.StreamHandler()
-    console.setLevel(logging.DEBUG if debug else logging.INFO)
-    formatter = logging.Formatter(frmt_str)
-    console.setFormatter(formatter)
-    logger.addHandler(console)
-
-    return logger
-
 LOG = configure_logging(debug = config.verbose, filename = "logfile.log")
 
 # Optional code for integration with CloudLab
---
+++ logger.py
@@ -0,0 +1,32 @@
+import logging
+
+########################
+### Configure Log file ###
+########################
+def configure_logging(debug=False, filename='mylog.log'):
+    """ This function configures logging facility.
+    The current setup is for printing log messages onto console AND onto the file.
+    Formatters are the same for both output destinations.
+    Handing of log levels:
+    - console output includes DEBUG messages or not depending on the `debug` argument.
+    - file ouput includes all levels including DEBUG.
+    """
+    frmt_str = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+    frmt_out = '%(message)s'
+
+    # set up logging to file
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(filename)
+    f_formatter = logging.Formatter(frmt_str)
+    file_handler.setFormatter(f_formatter)
+    logger.addHandler(file_handler)
+
+    # define a handler for console
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG if debug else logging.INFO)
+    formatter = logging.Formatter(frmt_str)
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+
+    return logger
