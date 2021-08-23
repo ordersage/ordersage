79870e452543004d6867e2348c5365b00cdadf7e
Carina Imburgia <Carina.Imburgia@utah.edu>
2021-08-22 22:47:17 -0600

    Added to stats

--- stats.py
+++ stats.py
@@ -7,10 +7,12 @@ import statistics as stat
 import scipy.stats as stats
 
 #TODO:
-#       exclude runs that had a failure 
+#       get data set with known results for testing
+#       exclude runs that had a failure
 #       test each node individually, then together and compare results
 #       Configuration: group_by should include HW type
-#       configure CI testing
+#           listresources command for CL, look into others
+#       configure CI testing (waiting on code from Nikhil)
 #       produce a report of statistical results
 
 def epoch2human(epoch):
@@ -26,6 +28,7 @@ df_env = pd.read_csv(glob.glob(time + "_results/*_all_env_out.csv")[0])
 # Split by rand vs seq
 df_exp_rand = df_exp[df_exp['order_type'] == 'random']
 df_exp_seq = df_exp[df_exp['order_type'] == 'fixed']
+
 """##SHAPIRO WILK TEST"""
 
 def SW_test(df,measure,columns):
@@ -120,147 +123,3 @@ pos = tmp[tmp["Pos_or_Neg"]== "positive"]
 pos_neg = pd.DataFrame(columns=["Num_seq_better_rand","Median","90th","Num_rand_better_seq","neg_Median","neg_90th"])
 pos_neg.loc[len(pos_neg)] = [len(neg),stat.median(neg["abs_P_Diff"].values), neg["abs_P_Diff"].quantile(0.9),len(pos),stat.median(pos["abs_P_Diff"].values),pos["abs_P_Diff"].quantile(0.9)]
 print(pos_neg)
-
-# """#Exploring sequences in detail
-#
-# ##Extracting the necessary data
-# """
-#
-# cpu_test = ["LU","CG","UA","MG","FT","BT","EP","SP","IS"]
-# def check_test(name):
-#   if(name in cpu_test):
-#     return 1
-#   else:
-#     return 0
-#
-# mem_t = ["add", "add_omp","read_memory_loop_omp" , "write_memory_rep_stosq_omp" , "read_memory_rep_lodsq"  , "write_memory_sse_omp" , "write_memory_loop_omp" , "write_memory_loop" , "write_memory_avx_omp" , "read_memory_sse_omp" , "write_memory_memset_omp" , "write_memory_nontemporal_avx"  , "write_memory_rep_stosq" , "read_memory_rep_lodsq_omp" , "read_memory_prefetch_avx_omp" , "read_memory_loop" , "read_memory_avx_omp" , "write_memory_nontemporal_avx_omp" , "read_memory_sse" , "read_memory_prefetch_avx" , "read_memory_avx" , "write_memory_sse" , "write_memory_avx" , "write_memory_nontemporal_sse_omp" , "write_memory_nontemporal_sse", "write_memory_memset"]
-#
-# x = mem_cpu["random_ops"].head(1)
-# for i in x:
-#   print(i)
-#
-# def is_nan(x):
-#     return (x != x)
-#
-# mem_key = dict({"rx": "read_memory_rep_lodsq", "rl": "read_memory_loop" , "rs": "read_memory_sse", "rv": "read_memory_avx", "rvn": "read_memory_prefetch_avx" , "wl": "write_memory_loop", "wx": "write_memory_rep_stosq" , "ws": "write_memory_sse", "wsn": "write_memory_nontemporal_sse" , "wv": "write_memory_avx", "wvn": "write_memory_nontemporal_avx" , "wm": "write_memory_memset", "orx": "read_memory_rep_lodsq_omp" , "orl": "read_memory_loop_omp" , "ors": "read_memory_sse_omp" , "orv": "read_memory_avx_omp", "orvn": "read_memory_prefetch_avx_omp" , "owl": "write_memory_loop_omp" , "owx": "write_memory_rep_stosq_omp" , "ows": "write_memory_sse_omp" , "owsn": "write_memory_nontemporal_sse_omp" , "owv": "write_memory_avx_omp" , "owvn": "write_memory_nontemporal_avx_omp" , "owm": "write_memory_memset_omp"})
-#
-# import re
-# mem_cpu = mem_with_random.append(cpu_with_random)
-# mem_cpu =mem_cpu.reset_index(drop=True)
-#
-# tmem_cpu = pd.DataFrame()
-# whitelist = set('abcdefghijklmnopqrstuvwxyz')
-#
-# for idx,grp in mem_cpu.groupby("run_uuid"):
-#   a = grp.random_ops.values[0]
-#
-#   if(is_nan(a)):
-#     continue
-#
-#   a = a[1:(len(a)-1)]
-#   a = a.split("), ")
-#   tmp_a = []
-#   flag = 0
-#   param = []
-#   if "membench" in a[0]:
-#
-#     mem_temp = a[0].split()
-#     mem_test =  ''.join(filter(whitelist.__contains__, mem_temp[3]))
-#     mem_test = mem_key.get(mem_test,"NaN")
-#     flag = 1
-#
-#   elif "stream" in a[0]:
-#     flag = 1
-#     if "ST" in a[0]:
-#       mem_test = "add"
-#     else:
-#       mem_test = "add_omp"
-#
-#   else:
-#     continue
-#
-#   if flag and "npb" in a[1]:
-#
-#     cpu_temp =  a[1].split(",")
-#     cpu_test =  ''.join(filter(whitelist.__contains__, cpu_temp[1]))
-#     cpu_test = cpu_test.upper()
-#     if(cpu_temp[3][2]== '0'):
-#       dvfs = "no"
-#     else:
-#       dvfs = "yes"
-#     if(cpu_temp[2][2]== 'MT'):
-#       thread = 1
-#     else:
-#       thread = 0
-#       param = [dvfs,int(cpu_temp[4][2]),thread]
-#   else:
-#     flag = 0
-#
-#   c = 0
-#
-#   if(flag):
-#       ind = grp.loc[(grp["testname"]== cpu_test) & (grp["socket_num"] == param[1]) & (grp["dvfs"]== param[0]) & (grp["MT"]== param[2])].index.tolist()
-#       tmem_cpu = tmem_cpu.append(mem_cpu.loc[ind])
-#       tmem_cpu.at[tmem_cpu.index[-1], 'random_ops']  = str(tmp_a)
-#       tmem_cpu.at[tmem_cpu.index[-1],'TEST'] = mem_test
-#
-# display(len(tmem_cpu))
-#
-# baseline2 = {}
-# for idx, grp in cpu_with_random.groupby("testname"):
-#   baseline2[idx] = stat.stdev(grp["exec_time"].values)
-#
-#
-# cpu_t = sorted(baseline2.items(), key=lambda x: x[1])
-#
-# """##Plotting the performance"""
-#
-# accurate_mem_cpu = tmem_cpu
-#
-# #The red dotted line is the mean of all the execution times in the data set(cpu_with_random) of the
-# #particular CPU test the graph represents
-#
-# df_vals = pd.DataFrame(columns=["Testname","Test","mean","diff_mean","median","no_of_datapoints"])
-#
-# for idx in cpu_t:
-#   grp = accurate_mem_cpu[accurate_mem_cpu["testname"]==idx[0]]
-#   fig, (ax) = plt.subplots(1, 1, figsize=(16,4))
-#   grp1 = cpu_with_random[cpu_with_random["testname"]==idx[0]]
-#   data = pd.DataFrame(columns=["TEST","Vals"])
-#   overall_mean = stat.mean(grp1.exec_time.values)
-#
-#   for i in grp["TEST"].unique():
-#     for j in list(grp[grp["TEST"]==i]["exec_time"].values):
-#       data.loc[len(data)] = [i,j]
-#     current_mean = grp[grp["TEST"]==i]["exec_time"].mean(axis=0)
-#     diff = overall_mean -current_mean
-#     df_vals.loc[len(df_vals)] = [idx[0], i, current_mean, diff , stat.median(d),len(d)]
-#
-#   #plotting the data
-#   ax = sns.swarmplot(x="TEST", y="Vals", data=data, size=3)
-#   ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
-#   ax.set_title((idx[0]))
-#   ax.axhline(overall_mean, ls='-.',color = 'r')
-#   ax.set_ylabel("execution time")
-#   plt.show();
-#   print("\n\n")
-#   print("\n\n \t\t\t\t\t\t-------NEXT CPU TEST-------- \n")
-#
-# """##Analysing"""
-#
-# df_vals["abs_diff_mean"] = df_vals["diff_mean"].apply(abs)
-# df_vals = df_vals.sort_values("abs_diff_mean",ascending= False)
-#
-# #Here for each CPU test, I select the memory test(i.e the memory test after hwich this cpu test runs)
-# #that differs the furthest from the mean value of execution time of the cpu test
-#
-#
-# maxtest = []
-# for idx,grp in df_vals.groupby("Testname"):
-#   maxtest.append((idx,grp.Test.values[0],grp.abs_diff_mean.values[0],grp.no_of_datapoints.values[0]))
-#
-#
-# for i in maxtest:
-#   print(i,"\n")
-#
-# """#TESTING#"""
